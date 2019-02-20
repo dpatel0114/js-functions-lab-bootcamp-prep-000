fa6b008610b4a9d4bc89b30d65b61a7e416568fb
Dhara Patel <1991dharapatel@gmail.com>
2019-02-20 16:54:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,7 @@
 function happyHolidays(){
   return "Happy holidays!"
 }
-function happyHolidaysTo(you) 
-function happyHolidaysTo(holiday, you){
+function happyHolidaysTo(you) {
   return `Happy holidays, ${you}!`
-  return `Happy ${holiday},${}`
 }
 
