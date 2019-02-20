5d79a61878340a61b3f3bf0bca4d030b1bce3cd8
Dhara Patel <1991dharapatel@gmail.com>
2019-02-20 16:21:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+function happyHolidayTo(holiday, name) {
+  return '${Happy Independence day}, ${krunal}!`
+  console.log{`Happy Independence Day!`}
+}
