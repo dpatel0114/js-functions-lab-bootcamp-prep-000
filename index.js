b4ec27e72e62c2232a994565a894ae85399c6fb0
Dhara Patel <1991dharapatel@gmail.com>
2019-02-20 17:03:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ function happyHolidaysTo(you) {
   return `Happy holidays, ${you}!`
 }
 
-function holidayCountdown(Mother's Day, 20){
-return "It's ${20} days until${Mother's Day}!"}
+function holidayCountdown(Mother Day, 20)
+{
+return "It's ${20} days until${Mother Day}!"
+}
